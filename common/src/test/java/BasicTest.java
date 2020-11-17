81020ad1e99a0180ca9a8306eca12ec688fe28fa
David Vasiliauskas <3217309-david.vasiliauskas@users.noreply.gitlab.com>
2020-11-17 04:09:39 +0000

    Project setup

---
+++ common/src/test/java/BasicTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BasicTest {
+
+    @Test
+    void testAdditionOperation(){
+        assertEquals(2 + 2, 4);
+    }
+}
