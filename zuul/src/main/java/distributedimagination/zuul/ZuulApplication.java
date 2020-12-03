e93799d759df42b6cb82330884d32da0b12e9e2e
Jaap <jaapetto@gmail.com>
2020-12-03 17:28:42 +0000

    Zuul now communicates with Eureka server

--- zuul/src/main/java/distributedimagination/zuul/ZuulApplication.java
+++ zuul/src/main/java/distributedimagination/zuul/ZuulApplication.java
@@ -2,8 +2,12 @@ package distributedimagination.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
