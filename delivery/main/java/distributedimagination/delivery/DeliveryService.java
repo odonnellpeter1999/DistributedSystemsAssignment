262428bc4e05bb802f75addc24e5ecbe025cc0e4
magnussanne <magnussanne@me.com>
2020-12-22 22:36:28 +0000

    base services

---
+++ delivery/main/java/distributedimagination/delivery/DeliveryService.java
@@ -0,0 +1,79 @@
+package distributedimagination.delivery;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.ws.rs.GET;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class QuotationService {
+
+    public ArrayList<InstanceInfo> applicationsInstances = new ArrayList<>();
+    @Qualifier("eurekaClient")
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${server.port}")
+    private String portNumber;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryService.class, args);
+    }
+
+
+    @RequestMapping("/service-instances/list")
+    public ArrayList<InstanceInfo> serviceInstancesList() {
+        List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
+        for (Application application : applications) {
+            applicationsInstances = (ArrayList<InstanceInfo>) application.getInstances();
+            for (InstanceInfo applicationsInstance : applicationsInstances) {
+                String name = applicationsInstance.getAppName();
+                String url = applicationsInstance.getHomePageUrl();
+                System.out.println(name + ": " + url);
+            }
+        }
+        return applicationsInstances;
+    }
+
+    @RequestMapping("/service-instances/delivery")
+    public ArrayList<String> getDeliveryList(@PathVariable String applicationName) {
+        ArrayList<String> delivery = new ArrayList<String>();
+        for (int i=0; i < applicationsInstances.size();i++){
+           applicationName = applicationsInstances.get(i).getAppName();
+           delivery.add("/{applicationName}/deliverydate");
+        }
+        return delivery;
+    }
+
+
+
+
+
+
+
+
+}
+
