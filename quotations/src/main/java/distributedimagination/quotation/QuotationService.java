6307262da4e9e19208a4b4afd40f5d3ba255c8a7
Finbar <finbarday@gmail.com>
2020-12-26 21:25:15 +0000

    new method of finding instances

--- quotations/src/main/java/distributedimagination/quotation/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/QuotationService.java
@@ -1,10 +1,8 @@
 package distributedimagination.quotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@EnableDiscoveryClient
 @SpringBootApplication
 
 public class QuotationService {
