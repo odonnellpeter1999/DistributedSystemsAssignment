8dfe6fd5adfdd70eed022b6c449b586405800d45
tommy1011 <thomas.reilly@ucdconnect.ie>
2020-12-27 19:45:28 +0000

    Modified RegistryService to just return a list of URL's without any duplicates.

--- discovery/src/main/java/distributedimagination/eureka/service/RegistryService.java
+++ discovery/src/main/java/distributedimagination/eureka/service/RegistryService.java
@@ -14,29 +14,17 @@ public class RegistryService {
     @Autowired
     private EurekaClient eurekaClient;
 
-    public List<InstanceInfo> getPostalServices() {
-        Set<InstanceInfo> postalServices = new HashSet<>();
+    public List<String> getPostalServices() {
+        Set<String> postalServices = new HashSet<>();
         List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
         for (Application application : applications) {
             for (InstanceInfo instance: application.getInstances()) {
-                if (instance.getAppName().contains("POSTALSERVICE"))
-                    postalServices.add(instance);
+                if (instance.getAppName().contains("POSTAL-SERVICE"))
+                    postalServices.add(instance.getHomePageUrl());
             }
 
         }
-        return new ArrayList<InstanceInfo>(postalServices);
-    }
-
-    public Map<String, String> getPostalURLs() {
-        Map<String, String> postalServices = new HashMap<>();
-        List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
-        for (Application application : applications) {
-            for (InstanceInfo instance: application.getInstances()) {
-                if (instance.getAppName().contains("POSTALSERVICE"))
-                    postalServices.put(instance.getMetadata().get("service-name"), instance.getHomePageUrl());
-            }
-        }
-        return postalServices;
+        return new ArrayList<String>(postalServices);
     }
 
 }
