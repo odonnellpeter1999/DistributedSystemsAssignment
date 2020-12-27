c6d029a472d84ee6ae1c92e0f198478efe9382fd
tommy1011 <thomas.reilly@ucdconnect.ie>
2020-12-27 17:05:19 +0000

    Converted demo-service a registry-client service. Added an endpoint to registry-client service which looks up postal service clients.

--- registry-client/src/main/java/com/services/registryclient/ServiceApplication.java
+++ registry-client/src/main/java/com/services/registryclient/ServiceApplication.java
@@ -1,4 +1,4 @@
-package com.services.demoservice;
+package com.services.registryclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
