0697143ad96a7b12ba328bef7ee7bf88508f6cb5
Finbar <finbarday@gmail.com>
2020-12-27 18:01:04 +0000

    re implementing quotations

--- quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
+++ quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
@@ -1,14 +1,13 @@
 package distributedimagination.quotation.controller;
 
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -16,23 +15,22 @@ public class QuotationController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    public ArrayList<String> applicationsInstances = new ArrayList<>();
-
-
+    public ArrayList<String> serviceInstances = new ArrayList<>();
 
     @RequestMapping(value = "/service-instances/list")
     public ArrayList getApplications() {
-        applicationsInstances.addAll(discoveryClient.getServices());
-        return applicationsInstances;
+        serviceInstances.addAll(discoveryClient.getServices());
+        return serviceInstances;
+    }
+
+    @RequestMapping("/service-instances/quotations")
+    public ArrayList<String> getQuotationsList(@PathVariable String applicationName) {
+        ArrayList<String> quotations = new ArrayList<String>();
+        quotations.add("Sample Quote");
+        for (int i = 0; i < serviceInstances.size(); i++) {
+            applicationName = serviceInstances.get(i);
+            quotations.add("/{applicationName}/quote");
+        }
+        return quotations;
     }
 }
-
-//    @RequestMapping("/service-instances/quotations")
-//    public ArrayList<String> getQuotationsList(@PathVariable String applicationName) {
-//        ArrayList<String> quotations = new ArrayList<String>();
-//        for (int i = 0; i < applicationsInstances.size(); i++) {
-//            applicationName = applicationsInstances.get(i).getAppName();
-//            quotations.add("/{applicationName}/quote");
-//        }
-//        return quotations;
-//    }
