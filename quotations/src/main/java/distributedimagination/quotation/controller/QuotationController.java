24dd3f9e0b1b25704b134ddc3e29b0fc9fea3242
Finbar <finbarday@gmail.com>
2020-12-27 18:13:31 +0000

    added get request for postal services quotes

--- quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
+++ quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
@@ -3,10 +3,16 @@ package distributedimagination.quotation.controller;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 @SpringBootApplication
@@ -24,11 +30,17 @@ public class QuotationController {
     }
 
     @RequestMapping("/service-instances/quotations")
-    public ArrayList<String> getQuotationsList(@PathVariable String applicationName) {
+    public ArrayList<String> getQuotationsList(@PathVariable String applicationName) throws URISyntaxException {
         ArrayList<String> quotations = new ArrayList<String>();
         quotations.add("Sample Quote");
         for (int i = 0; i < serviceInstances.size(); i++) {
+            if(serviceInstances.get(i).contains("POSTAL-SERVICE"))
             applicationName = serviceInstances.get(i);
+            final String baseUrl = "http://localhost:"+8761+"/{applicationName}/quote";
+            URI uri = new URI(baseUrl);
+            RestTemplate restTemplate = new RestTemplate();
+            HttpEntity<String> requestEntity = new HttpEntity<>(null);
+            ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET,requestEntity, String.class);
             quotations.add("/{applicationName}/quote");
         }
         return quotations;
