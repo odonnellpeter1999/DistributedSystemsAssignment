32df022eeb3a9ee62996fc77221001edcae14a29
Finbar <finbarday@gmail.com>
2020-12-29 19:32:20 +0000

    works with new map

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -2,24 +2,16 @@ package distributedimagination.quotation.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class QuotationService {
 
-    public Map<String, String > getQuotes() {
+    public Map<String, String> getQuotes() {
         final String uri = "http://discovery:8761/postal-services/urls";
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        Map<String, String> map = new HashMap<String, String>();
-        String[] pairs = result.split(",");
-        for (int i=0;i<pairs.length;i++) {
-            String pair = pairs[i];
-            String[] keyValue = pair.split(":");
-            map.put(keyValue[0], keyValue[1]);
-        }
+        Map<String, String> map;
+        map = restTemplate.getForObject(uri, Map.class);
         return map;
     }
 }
