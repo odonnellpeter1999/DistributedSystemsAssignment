868430d0bfa416e798be35be610d75bb39769ca5
tommy1011 <thomas.reilly@ucdconnect.ie>
2020-12-29 22:47:19 +0000

    Added common.PostalServiceInstance to common module. This should allow for easier parsing of InstanceInfo objects returned by discovery.

--- discovery/src/main/java/distributedimagination/eureka/service/RegistryService.java
+++ discovery/src/main/java/distributedimagination/eureka/service/RegistryService.java
@@ -5,6 +5,7 @@ import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import common.PostalServiceInstances;
 
 import java.util.*;
 
@@ -14,7 +15,7 @@ public class RegistryService {
     @Autowired
     private EurekaClient eurekaClient;
 
-    public List<InstanceInfo> getPostalServices() {
+    public PostalServiceInstances getPostalServiceInstances() {
         Set<InstanceInfo> postalServices = new HashSet<>();
         List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
         for (Application application : applications) {
@@ -22,9 +23,20 @@ public class RegistryService {
                 if (instance.getAppName().contains("POSTALSERVICE"))
                     postalServices.add(instance);
             }
-
         }
-        return new ArrayList<InstanceInfo>(postalServices);
+        return new PostalServiceInstances(new ArrayList<InstanceInfo>(postalServices));
+    }
+
+    public Map<String, String> getPostalURLs() {
+        Map<String, String> postalServices = new HashMap<>();
+        List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
+        for (Application application : applications) {
+            for (InstanceInfo instance: application.getInstances()) {
+                if (instance.getAppName().contains("POSTALSERVICE"))
+                    postalServices.put(instance.getMetadata().get("service-name"), instance.getHomePageUrl());
+            }
+        }
+        return postalServices;
     }
 
 }
