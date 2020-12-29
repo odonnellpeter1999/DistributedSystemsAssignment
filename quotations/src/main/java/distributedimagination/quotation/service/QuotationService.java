5dea79b46838177f8ecd1a99f8dcacb36a62f2bb
Finbar <finbarday@gmail.com>
2020-12-29 19:45:28 +0000

    refactored and rewrote get quotations, need to test with real output

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -1,7 +1,16 @@
 package distributedimagination.quotation.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map;
 
 @Service
@@ -13,5 +22,19 @@ public class QuotationService {
         Map<String, String> map;
         map = restTemplate.getForObject(uri, Map.class);
         return map;
+    }
+
+    public ArrayList<String> getQuotationsList() {
+        ArrayList<String> quotations = new ArrayList<>();
+        Map<String, String> map = getQuotes();
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = iterator.next();
+            String appURL = entry.getValue();
+            String name = entry.getKey();
+            RestTemplate restTemplates = new RestTemplate();
+            quotations.add(restTemplates.getForObject(appURL, String.class));
+        }
+        return quotations;
     }
 }
