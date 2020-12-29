19bd6934e7ca1b3d53bd774b50903bf75eafe99f
Finbar <finbarday@gmail.com>
2020-12-29 17:42:56 +0000

    refactoring to controller and services package

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -1,33 +1,19 @@
 package distributedimagination.quotation.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import java.util.ArrayList;
-import java.util.Iterator;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Map;
 
 @Service
 public class QuotationService {
 
-    public Map<String, String> getQuotes() {
-        final String uri = "http://discovery:8761/postal-services/urls";
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, String> map;
-        map = restTemplate.getForObject(uri, Map.class);
-        return map;
-    }
-
-    public ArrayList<String> getQuotationsList() {
-        ArrayList<String> quotations = new ArrayList<>();
-        Map<String, String> map = getQuotes();
-        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, String> entry = iterator.next();
-            String appURL = entry.getValue() + "/quote";
-            String name = entry.getKey();
-            RestTemplate restTemplates = new RestTemplate();
-            quotations.add(restTemplates.getForObject(appURL, String.class));
-        }
-        return quotations;
+    @RequestMapping(value = "http://discovery:8761/postal-services/urls", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, String> getMap(@RequestParam Map<String, String> allParams) {
+        return allParams;
     }
 }
