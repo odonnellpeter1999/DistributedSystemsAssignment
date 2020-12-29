f9e2b548ea1ff3203ba82aaa179dadb54b698536
Finbar <finbarday@gmail.com>
2020-12-29 18:08:37 +0000

    changed to get request

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -1,17 +1,14 @@
 package distributedimagination.quotation.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Service
 public class QuotationService {
 
-    @RequestMapping(value = "http://discovery:8761/postal-services/urls", method = RequestMethod.GET)
+    @GetMapping("http://discovery:8761/postal-services/urls")
     @ResponseBody
     public Map<String, String> getMap(@RequestParam Map<String, String> allParams) {
         return allParams;
