0c82711d5fbe1c63117d5aef2949eae044dcee0f
Finbar <finbarday@gmail.com>
2020-12-29 16:21:55 +0000

    trying to map values

--- quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
+++ quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
@@ -1,7 +1,10 @@
 package distributedimagination.quotation.controller;
+
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
+
+import jdk.nashorn.internal.parser.JSONParser;
+import org.h2.util.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpEntity;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.json.*;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -21,40 +27,54 @@ public class QuotationController {
     @Autowired
     private EurekaClient eurekaClient;
 
-    @RequestMapping(value = "/service-instances/list")
-    public ArrayList<String> getApplications() {
-        ArrayList<String> serviceInstances = new ArrayList<String>();
-        List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
-        for (Application application : applications) {
-            for (InstanceInfo instance : application.getInstances()) {
-                if (instance.getAppName().contains("POSTAL-SERVICE-"))
-                    serviceInstances.add(instance.getHomePageUrl());
-            }
-        }
-        return serviceInstances;
-    }
+//    @RequestMapping(value = "/service-instances/list")
+//    public ArrayList<String> getApplications() {
+//        ArrayList<String> serviceInstances = new ArrayList<String>();
+//        List<Application> applications = eurekaClient.getApplications().getRegisteredApplications();
+//        for (Application application : applications) {
+//            for (InstanceInfo instance : application.getInstances()) {
+//                if (instance.getAppName().contains("POSTAL-SERVICE-"))
+//                    serviceInstances.add(instance.getHomePageUrl());
+//            }
+//        }
+//        return serviceInstances;
+//    }
 
     @RequestMapping(value = "/service-instances/quotations")
     public ArrayList<String> getQuotationsList() throws URISyntaxException {
 
         ArrayList<String> quotations = new ArrayList<>();
-        ArrayList<InstanceInfo> instances = new ArrayList<>();
+        ArrayList<String> instances = new ArrayList<>();
+
         String baseUrl = "http://discovery:8761/postal-services";
         URI uri = new URI(baseUrl);
         RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<InstanceInfo> requestEntity = new HttpEntity<>(null);
-        ResponseEntity<InstanceInfo> result = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, InstanceInfo.class);
-        InstanceInfo mapped = result.getBody();
-        instances.add(mapped);
+        HttpEntity<String> requestEntity = new HttpEntity<>(null);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
 
-        Iterator<InstanceInfo> iterator = instances.iterator();
+        Map<String, String> map = Arrays.stream(result.toString().split(","))
+                .map(s -> s.split(":"))
+                .collect(Collectors.toMap(s -> s[0], s -> s[1]));
+
+        quotations.add(map.get("homePageUrl"));
+
+
+
+        String instance = parse.getFirst("homePageUrl").toString();
+
+        instances.add(instance);
+
+        Iterator<String> iterator = instances.iterator();
 
         while (iterator.hasNext()) {
-            String appURL = iterator.next().getHomePageUrl();
+            String appURL = iterator.next();
             baseUrl = appURL + "/quote";
             uri = new URI(baseUrl);
-            ResponseEntity<String> quote = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
-            quotations.add(quote.toString());
+            RestTemplate restTemplates = new RestTemplate();
+            HttpEntity<String> requestEntitys = new HttpEntity<>(null);
+            ResponseEntity<String> quoteResponse = restTemplates.exchange(uri, HttpMethod.GET, requestEntitys, String.class);
+            String quote = quoteResponse.getBody();
+            quotations.add(quote);
         }
         return quotations;
     }
