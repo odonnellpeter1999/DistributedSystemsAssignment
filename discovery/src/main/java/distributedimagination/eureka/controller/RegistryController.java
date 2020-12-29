b45650381f4e3f1a2c29d6e78a12c89b7da14b29
tommy1011 <thomas.reilly@ucdconnect.ie>
2020-12-29 16:38:33 +0000

    Added /postal-services/urls route for returning just the homepage URLs of each postal service.

--- discovery/src/main/java/distributedimagination/eureka/controller/RegistryController.java
+++ discovery/src/main/java/distributedimagination/eureka/controller/RegistryController.java
@@ -4,14 +4,12 @@ import com.netflix.appinfo.InstanceInfo;
 import distributedimagination.eureka.service.RegistryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/postal-services", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RegistryController {
 
     private final RegistryService registryService;
@@ -21,8 +19,14 @@ public class RegistryController {
         this.registryService = registryService;
     }
 
-    @GetMapping
-    public List<InstanceInfo> getAllParcels() {
+    @RequestMapping(value = "/postal-services", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<InstanceInfo> getPostalServiceInstances() {
         return registryService.getPostalServices();
     }
+
+    @RequestMapping(value = "/postal-services/urls", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getPostalServiceURLs() {
+        return registryService.getPostalURLs();
+    }
+
 }
