e129dfdd5aeb86fb2d3230d2bcc6e0e3c833cad8
Finbar <finbarday@gmail.com>
2020-12-29 19:22:27 +0000

    change to map

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -1,17 +1,25 @@
 package distributedimagination.quotation.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class QuotationService {
 
-    @GetMapping("http://discovery:8761/postal-services/urls")
-    @ResponseBody
-    public String getQuotes() {
-        return "";
+    public Map<String, String > getQuotes() {
+        final String uri = "http://discovery:8761/postal-services/urls";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        Map<String, String> map = new HashMap<String, String>();
+        String[] pairs = result.split(",");
+        for (int i=0;i<pairs.length;i++) {
+            String pair = pairs[i];
+            String[] keyValue = pair.split(":");
+            map.put(keyValue[0], keyValue[1]);
+        }
+        return map;
     }
 }
