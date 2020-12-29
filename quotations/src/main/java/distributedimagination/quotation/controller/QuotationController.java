5cca63454abb9509fc586cb54b5e20752a7dd1d3
Finbar <finbarday@gmail.com>
2020-12-29 19:32:20 +0000

    works with new map

--- quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
+++ quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
@@ -30,31 +30,31 @@ public class QuotationController {
         return quotationService.getQuotes();
     }
 
-//    @RequestMapping(value = "/service-instances/quotations")
-//    public ArrayList<String> getQuotationsList() throws URISyntaxException {
-//        ArrayList<String> quotations = new ArrayList<>();
-//        ArrayList<String> instances = new ArrayList<>();
-//
-//        String baseUrl = "http://discovery:8761/postal-services/urls";
-//        URI uri = new URI(baseUrl);
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpEntity<String> requestEntity = new HttpEntity<>(null);
-//
-//        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
-//        String lines[] = result.toString().split("\\r?\\n");
-//        instances.add(Arrays.lines.asList());
-//
-//        Iterator<String> iterator = instances.iterator();
-//        while (iterator.hasNext()) {
-//            String appURL = iterator.next();
-//            baseUrl = appURL + "/quote";
-//            uri = new URI(baseUrl);
-//            RestTemplate restTemplates = new RestTemplate();
-//            HttpEntity<String> requestEntitys = new HttpEntity<>(null);
-//            ResponseEntity<String> quoteResponse = restTemplates.exchange(uri, HttpMethod.GET, requestEntitys, String.class);
-//            String quote = quoteResponse.getBody();
-//            quotations.add(quote);
-//        }
-//        return quotations;
-//    }
+    @RequestMapping(value = "/service-instances/quotations")
+    public ArrayList<String> getQuotationsList() throws URISyntaxException {
+        ArrayList<String> quotations = new ArrayList<>();
+        ArrayList<String> instances = new ArrayList<>();
+
+        String baseUrl = "http://discovery:8761/postal-services/urls";
+        URI uri = new URI(baseUrl);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> requestEntity = new HttpEntity<>(null);
+
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
+        String lines[] = result.toString().split("\\r?\\n");
+        instances.add(Arrays.lines.asList());
+
+        Iterator<String> iterator = instances.iterator();
+        while (iterator.hasNext()) {
+            String appURL = iterator.next();
+            baseUrl = appURL + "/quote";
+            uri = new URI(baseUrl);
+            RestTemplate restTemplates = new RestTemplate();
+            HttpEntity<String> requestEntitys = new HttpEntity<>(null);
+            ResponseEntity<String> quoteResponse = restTemplates.exchange(uri, HttpMethod.GET, requestEntitys, String.class);
+            String quote = quoteResponse.getBody();
+            quotations.add(quote);
+        }
+        return quotations;
+    }
 }
