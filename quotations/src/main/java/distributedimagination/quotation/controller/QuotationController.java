56fe9d3d108e4d7069d734af443f807976dd9e3c
Finbar <finbarday@gmail.com>
2020-12-29 16:23:10 +0000

    formatting

--- quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
+++ quotations/src/main/java/distributedimagination/quotation/controller/QuotationController.java
@@ -1,28 +1,49 @@
 package distributedimagination.quotation.controller;
 
-import distributedimagination.quotation.service.QuotationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
+import java.util.stream.Collectors;
 
-
+@SpringBootApplication
 @RestController
 public class QuotationController {
 
-    private final QuotationService quotationService;
-
-    @Autowired
-    public QuotationController(QuotationService quotationService) {
-        this.quotationService = quotationService;
-    }
-
-    @RequestMapping(value = "/service-instances/quotations/list")
-    public Map<String, String> returnMap() {
-        return quotationService.getQuotes();
-    }
-
     @RequestMapping(value = "/service-instances/quotations")
-    public ArrayList<String> getQuotationsList() {
-        return quotationService.getQuotationsList();
+    public ArrayList<String> getQuotationsList() throws URISyntaxException {
+        ArrayList<String> quotations = new ArrayList<>();
+        ArrayList<String> instances = new ArrayList<>();
+
+        String baseUrl = "http://discovery:8761/postal-services";
+        URI uri = new URI(baseUrl);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> requestEntity = new HttpEntity<>(null);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
+
+        Map<String, String> map = Arrays.stream(result.toString().split(","))
+                .map(s -> s.split(":"))
+                .collect(Collectors.toMap(s -> s[0], s -> s[1]));
+
+        instances.add(map.get("homePageUrl"));
+        Iterator<String> iterator = instances.iterator();
+
+        while (iterator.hasNext()) {
+            String appURL = iterator.next();
+            baseUrl = appURL + "/quote";
+            uri = new URI(baseUrl);
+            RestTemplate restTemplates = new RestTemplate();
+            HttpEntity<String> requestEntitys = new HttpEntity<>(null);
+            ResponseEntity<String> quoteResponse = restTemplates.exchange(uri, HttpMethod.GET, requestEntitys, String.class);
+            String quote = quoteResponse.getBody();
+            quotations.add(quote);
+        }
+        return quotations;
     }
 }
