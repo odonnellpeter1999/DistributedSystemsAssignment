3a88b508d6383bb3ecceebd8dea6da7c040c0760
magnussanne <magnussanne@me.com>
2020-12-30 14:03:34 +0000

    update

--- quotations/src/main/java/distributedimagination/quotation/QuotationApplication.java
+++ quotations/src/main/java/distributedimagination/quotation/QuotationApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class QuotationService {
+public class QuotationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuotationService.class, args);
     }
 
 }
-
--- tracking/main/java/distributedimagination/tracking/TrackingApplication.java
+++ tracking/main/java/distributedimagination/tracking/TrackingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrackingService {
+public class TrackingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrackingService.class, args);
