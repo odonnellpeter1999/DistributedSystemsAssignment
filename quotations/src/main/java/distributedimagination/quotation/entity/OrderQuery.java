f4b3900c55308f84ca7b66d02920fa0da5c89df6
Finbar <finbarday@gmail.com>
2020-12-31 20:07:46 +0000

    trying new method to post request with json

--- quotations/src/main/java/distributedimagination/quotation/entity/OrderQuery.java
+++ quotations/src/main/java/distributedimagination/quotation/entity/OrderQuery.java
@@ -1,4 +1,7 @@
 package distributedimagination.quotation.entity;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.List;
 
 
