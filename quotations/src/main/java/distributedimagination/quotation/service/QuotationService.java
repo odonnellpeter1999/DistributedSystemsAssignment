665cd43c2e2e5f0651ff8f791f09813443cc550e
Finbar <finbarday@gmail.com>
2020-12-31 19:00:34 +0000

    added entity to test quotes

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -1,13 +1,30 @@
 package distributedimagination.quotation.service;
 
+import distributedimagination.quotation.entity.OrderQuery;
+import distributedimagination.quotation.entity.ParcelQuery;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 
 @Service
 public class QuotationService {
+
+    private OrderQuery orderQuery;
+
+    public OrderQuery GenerateOrderTest() {
+        ParcelQuery testParcel = new ParcelQuery(100.0, 100.0, 100.0, 100.0);
+        ArrayList<ParcelQuery> parcelQueryList = new ArrayList<ParcelQuery>();
+        parcelQueryList.add(testParcel);
+        OrderQuery testOrder = new OrderQuery(100.0, 100.0, 100.0, 100.0,
+                parcelQueryList);
+
+        return testOrder;
+    }
 
     public Map<String, String> getQuotes() {
         final String uri = "http://discovery:8761/postal-services/urls";
@@ -17,16 +34,21 @@ public class QuotationService {
         return map;
     }
 
+
     public ArrayList<String> getQuotationsList() {
-        ArrayList<String> quotations = new ArrayList<>();
         Map<String, String> map = getQuotes();
+        orderQuery = GenerateOrderTest();
+        ArrayList<String> quotations = new ArrayList<>();
         Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, String> entry = iterator.next();
-            String appURL = entry.getValue() + "/quote";
             String name = entry.getKey();
+            quotations.add(entry.getValue());
+            String appURL = entry.getValue() + "quote";
+            quotations.add(name + ":" + appURL);
+            HttpEntity<String> requestEntity = new HttpEntity<String>(orderQuery.toString());
             RestTemplate restTemplates = new RestTemplate();
-            quotations.add(restTemplates.getForObject(appURL, String.class));
+            quotations.add(restTemplates.postForObject(appURL, requestEntity, String.class));
         }
         return quotations;
     }
