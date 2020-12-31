bc03414db95f879818db731f1b8316347d8a0204
Finbar <finbarday@gmail.com>
2020-12-31 19:00:34 +0000

    added entity to test quotes

---
+++ quotations/src/main/java/distributedimagination/quotation/entity/OrderQuery.java
@@ -0,0 +1,21 @@
+package distributedimagination.quotation.entity;
+import java.util.List;
+
+
+public class OrderQuery {
+
+    private Double sourceLon;
+    private Double sourceLat;
+    private Double destinationLon;
+    private Double destinationLat;
+    private List<ParcelQuery> parcels;
+
+    public OrderQuery(Double sourceLon, Double sourceLat, Double destinationLon, Double destinationLat, List<ParcelQuery> parcels) {
+        this.sourceLon = sourceLon;
+        this.sourceLat = sourceLat;
+        this.destinationLon = destinationLon;
+        this.destinationLat = destinationLat;
+        this.parcels = parcels;
+    }
+}
+
