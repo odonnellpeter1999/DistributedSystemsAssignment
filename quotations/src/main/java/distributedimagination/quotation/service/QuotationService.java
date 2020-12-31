4d1bfbd76d8d157e3f7a17d7443251eaac5beb0d
Finbar <finbarday@gmail.com>
2020-12-31 21:52:10 +0000

    Still can't send post request

--- quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
+++ quotations/src/main/java/distributedimagination/quotation/service/QuotationService.java
@@ -1,17 +1,18 @@
 package distributedimagination.quotation.service;
 
-import com.google.gson.Gson;
+
 import distributedimagination.quotation.entity.OrderQuery;
 import distributedimagination.quotation.entity.ParcelQuery;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
+
+import java.util.*;
 
 @Service
 public class QuotationService {
@@ -36,27 +37,28 @@ public class QuotationService {
         return map;
     }
 
-
-
-
     public ArrayList<String> getQuotationsList() {
         Map<String, String> map = getQuotes();
         ArrayList<String> quotations = new ArrayList<>();
         Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, String> entry = iterator.next();
-            RestTemplate restTemplates = new RestTemplate();
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
             String name = entry.getKey();
             String appURL = entry.getValue() + "quote";
 
-            Gson g = null;
-            String jsonString = g.toJson(orderQuery);
-            HttpEntity<String> request = new HttpEntity<>(jsonString);
-            ResponseEntity<String> responseEntity = restTemplates.postForEntity(appURL, request, String.class);
-
+            HttpEntity<String> request = new HttpEntity<String>(orderQuery.toString(), headers);
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(appURL, request, String.class);
             quotations.add(responseEntity.getBody());
         }
         return quotations;
     }
+
+
+
 }
