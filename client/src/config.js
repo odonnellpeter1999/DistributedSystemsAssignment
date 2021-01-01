cf9f07acc64a9b466b3bb31aea243a6c25afbefc
tommy1011 <thomas.reilly@ucdconnect.ie>
2021-01-01 20:18:36 +0000

    Init Commit

---
+++ client/src/config.js
@@ -0,0 +1,36 @@
+let config = {
+    /*Products*/
+    "products": [
+        {
+            "name": "Gala Apples (10 Pack)",
+            "price": 4,
+            "packageInfo": {
+                "weight": 2.2,
+                "length": 20,
+                "width": 20,
+                "height": 20
+            }
+        },
+        {
+            "name": "Gala Apples (50 Pack)",
+            "price": 18,
+            "packageInfo": {
+                "weight": 10.4,
+                "length": 100,
+                "width": 100,
+                "height": 100
+            }
+        },
+        {
+            "name": "Gala Apples (200 Pack)",
+            "price": 75,
+            "packageInfo": {
+                "weight": 41,
+                "length": 400,
+                "width": 400,
+                "height": 400
+            }
+        }
+    ]
+}
+export default config
