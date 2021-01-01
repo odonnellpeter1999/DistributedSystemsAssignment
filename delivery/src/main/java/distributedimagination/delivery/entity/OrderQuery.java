766b7ff5c0afe27f8abb2fe783d74fa36186d86e
magnussanne <magnussanne@me.com>
2021-01-01 18:21:39 +0000

    delivery update

---
+++ delivery/src/main/java/distributedimagination/delivery/entity/OrderQuery.java
@@ -0,0 +1,24 @@
+package distributedimagination.quotation.entity;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.List;
+
+
+public class OrderQuery {
+
+    private Double sourceLon;
+    private Double sourceLat;
+    private Double destinationLon;
+    private Double destinationLat;
+    private List<ParcelQuery> parcels;
+
+    public OrderQuery(Double sourceLon, Double sourceLat, Double destinationLon, Double destinationLat, List<ParcelQuery> parcels) {
+        this.sourceLon = sourceLon;
+        this.sourceLat = sourceLat;
+        this.destinationLon = destinationLon;
+        this.destinationLat = destinationLat;
+        this.parcels = parcels;
+    }
+}
+
--- delivery/src/main/java/distributedimagination/delivery/service/DeliveryService.java
+++ delivery/src/main/java/distributedimagination/delivery/service/DeliveryService.java
@@ -9,6 +9,18 @@ import java.util.Map;
 @Service
 public class DeliveryService {
 
+    private OrderQuery orderQuery = GenerateOrderTest();
+
+    public OrderQuery GenerateOrderTest() {
+        ParcelQuery testParcel = new ParcelQuery(100.0, 100.0, 100.0, 100.0);
+        ArrayList<ParcelQuery> parcelQueryList = new ArrayList<ParcelQuery>();
+        parcelQueryList.add(testParcel);
+        OrderQuery testOrder = new OrderQuery(90.0, 90.0, 90.0, 90.0,
+                parcelQueryList);
+
+        return testOrder;
+    }
+
     public Map<String, String> getDeliveryDate() {
         final String uri = "http://discovery:8761/postal-services/urls";
         RestTemplate restTemplate = new RestTemplate();
@@ -23,10 +35,10 @@ public class DeliveryService {
         Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, String> entry = iterator.next();
-            String appURL = entry.getValue() + "/deliveryDate";
+            String appURL = entry.getValue() + "deliveryDate";
             String name = entry.getKey();
-            RestTemplate restTemplates = new RestTemplate();
-            delivery.add(restTemplates.getForObject(appURL, String.class));
+            RestTemplate restTemplate = new RestTemplate();
+            delivery.add(restTemplate.getForObject(appURL, String.class));
         }
         return delivery;
     }
